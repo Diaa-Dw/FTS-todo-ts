94af4d4c0890aba48e40391a6bc5454a944c7359
Diaa-Dw <oava9944@gmail.com>
2025-04-15 12:42:27 +0300

    Add reusable FormInput component and style it

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "../styles/formInput.style.css";
+
+interface FormInputProps {
+  type: string;
+  className?: string;
+  icon: React.ReactNode;
+  placeholder: string;
+  required?: boolean;
+}
+
+const FormInput = ({
+  type,
+  className = "",
+  icon,
+  placeholder,
+  required = true,
+}: FormInputProps) => {
+  return (
+    <div className={`todo-list__input-container ${className}`}>
+      <label htmlFor='add_task'>{icon}</label>
+      <input
+        type={type}
+        name='add_task'
+        id='add_task'
+        placeholder={placeholder}
+        className='todo-list__input--add'
+        required={required}
+      />
+    </div>
+  );
+};
+
+export default FormInput;
