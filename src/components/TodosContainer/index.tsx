463768d75755f669ba500ef1e9321a897fb11589
Diaa-Dw <oava9944@gmail.com>
2025-04-17 02:26:13 +0300

    Add modal for todo deletion with confirmation buttons

--- src/components/TodosContainer/index.tsx
+++ src/components/TodosContainer/index.tsx
@@ -9,10 +9,20 @@ import TableHead from "../TableHead";
 import TableBody from "../TableBody";
 import tableAttributes from "../../data/tableAttibutes.data";
 import TableRow from "../TableRow";
+import Modal from "../Modal";
+import Button from "../Button";
 
 const TodosContanier = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+
   const { todos, isLoading, setTodos } = useFetchTodos();
+
+  const handleDeleteTodo = () => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== selectedId));
+    setIsModalOpen(false);
+  };
 
   return (
     <main className='todo-list'>
@@ -27,14 +37,46 @@ const TodosContanier = () => {
         />
       </section>
 
-      <TodoTableContainer todos={todos} isLoading={isLoading}>
+      <TodoTableContainer isLoading={isLoading}>
         <TableHead tableAttributes={tableAttributes} />
         <TableBody>
           {todos.map((todo) => (
-            <TableRow key={todo.id} todo={todo} setTodos={setTodos} />
+            <TableRow
+              key={todo.id}
+              todo={todo}
+              setTodos={setTodos}
+              setIsModalOpen={setIsModalOpen}
+              setSelectedId={setSelectedId}
+            />
           ))}
         </TableBody>
       </TodoTableContainer>
+
+      <Modal
+        title='Delete Todo'
+        onClose={() => setIsModalOpen(false)}
+        isOpen={isModalOpen}
+      >
+        <p>Are you sure you want to delete this task?</p>
+        <div className='modal__action-buttons'>
+          <Button
+            variant='outline'
+            size='lg'
+            type='button'
+            onClick={() => setIsModalOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant='danger'
+            size='lg'
+            type='button'
+            onClick={handleDeleteTodo}
+          >
+            Delete
+          </Button>
+        </div>
+      </Modal>
     </main>
   );
 };
