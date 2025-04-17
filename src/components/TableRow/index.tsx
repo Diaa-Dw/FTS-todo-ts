1be92e3c367f84345d49bf81e657f7c42f556243
Diaa-Dw <oava9944@gmail.com>
2025-04-18 01:54:07 +0300

    Implement editable task input in TableRow component and add corresponding styles

--- src/components/TableRow/index.tsx
+++ src/components/TableRow/index.tsx
@@ -2,6 +2,7 @@ import { FaCheck, FaHourglassHalf, FaTrash } from "react-icons/fa";
 import Button from "../Button";
 import "./tableRow.style.css";
 import { TableRowProps } from "./tableRow.type";
+import { useState } from "react";
 
 const TableRow = ({
   todo,
@@ -9,6 +10,8 @@ const TableRow = ({
   setIsModalOpen,
   setSelectedId,
 }: TableRowProps) => {
+  const [taskValue, setTaskValue] = useState(todo.todo);
+  const [isTaskClicked, setIsTaskClicked] = useState<boolean>(false);
   const { id, todo: task } = todo;
   const status = todo.completed ? "completed" : "pending";
 
@@ -25,12 +28,35 @@ const TableRow = ({
     setSelectedId(id);
     setIsModalOpen(true);
   };
+  //handler function to update todo when input loses focus
+  const handleBlur = () => {
+    if (taskValue !== task) {
+      setTodos((prev) =>
+        prev.map((todo) =>
+          todo.id === id ? { ...todo, todo: taskValue } : todo
+        )
+      );
+    }
+    setIsTaskClicked(false);
+  };
 
   return (
     <tr className={`todo-list__table-row ${status}`}>
       <td className='todo-list__table-cell'>{todo.id}</td>
-      <td className='todo-list__table-cell todo-list__table-cell--task'>
-        {task}
+      <td
+        className='todo-list__table-cell todo-list__table-cell--task'
+        onClick={() => setIsTaskClicked(true)}
+      >
+        {isTaskClicked ? (
+          <input
+            value={taskValue}
+            onChange={(e) => setTaskValue(e.target.value)}
+            className='todo-list__table-cell-input'
+            onBlur={handleBlur}
+          />
+        ) : (
+          task
+        )}
       </td>
       <td className='todo-list__table-cell'>
         <span className='todo-list__task-status'>{status}</span>
